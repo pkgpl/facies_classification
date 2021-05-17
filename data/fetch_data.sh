e905912bf6040f0039b9d1820838fc8f4d1e3604
Wansoo Ha <wansooha@gmail.com>
2021-05-17 18:48:50 -0400

    env setting

---
+++ data/fetch_data.sh
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/seg/2016-ml-contest/master/facies_vectors.csv
--- docker/Env.sh
+++ docker/Env.sh
@@ -1,5 +1,5 @@
 # project name without space, lower case for image name
-PROJECT_NAME=my_project
+PROJECT_NAME=facies_ml
 
 # device: cpu or gpu
 DEVICE=gpu
